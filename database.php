7a7383b288cf9a98af35980ac1072487469ba156
samadhanMadane <32089599+samadhanMadane@users.noreply.github.com>
2017-09-19 14:24:34 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,4 @@
+<?php
+$cn=mysql_connect("ap-cdbr-azure-southeast-b.cloudapp.net","b68538c3a4a364","7efcd5a7") or die("Could not Connect My Sql");
+mysql_select_db("quiz_new",$cn)  or die("Could connect to Database");
+?>
